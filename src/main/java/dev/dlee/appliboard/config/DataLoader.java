4d4bdd3ff6716f31deb23817f8f2d121896d9e1b
daniel0802 <daniellee1105@gmail.com>
2024-08-23 11:38:21 -0700

    Added comments to methods in JobController

--- src/main/java/dev/dlee/appliboard/config/DataLoader.java
+++ src/main/java/dev/dlee/appliboard/config/DataLoader.java
@@ -2,8 +2,11 @@ package dev.dlee.appliboard.config;
 
 import org.springframework.boot.CommandLineRunner;
 
+// Class used for loading default data into database
+// CURRENTLY NOT BEING USED
 public class DataLoader implements CommandLineRunner {
 
+    // EFFECTS: Prints "Hello" to console
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello");
--- src/main/java/dev/dlee/appliboard/controller/JobListingController.java
+++ src/main/java/dev/dlee/appliboard/controller/JobListingController.java
@@ -11,9 +11,12 @@ import jakarta.validation.Valid;
 
 import java.util.List;
 
+// Implements and handles REST operations
+// Requests are made by adding /api/jobListing
 @RestController
 @RequestMapping("/api/jobListing")
 public class JobListingController {
+    // Repository initialization is handled by Spring
     private final JobListingRepository repository;
 
     @Autowired
@@ -21,22 +24,29 @@ public class JobListingController {
         this.repository = repository;
     }
 
+    // EFFECTS: Returns all JobListing elements from the repository in a List
     @GetMapping("")
     public List<JobListing> findAll() {
         return repository.findAll();
     }
 
+    // EFFECTS: If id is found, returns JobListing with matching id, else fails
+    //  THROWS: ResponseStatusException
     @GetMapping("/{id}")
     public JobListing findById(@PathVariable Integer id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    // EFFECTS: If request body is valid, creates a JobListing and saves it to the repository and returns
+    //          http status 201
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
     public void create(@Valid @RequestBody JobListing jobListing) {
         repository.save(jobListing);
     }
 
+    // EFFECTS: If request body and id are valid, replaces JobListing with matching id with the new JobListing in
+    //          the request body and returns http status 204
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void update(@RequestBody JobListing jobListing, @PathVariable Integer id) {
@@ -47,6 +57,8 @@ public class JobListingController {
         repository.save(jobListing);
     }
 
+    // EFFECTS: If the id is found, deletes the JobListing with matching id from the repository and returns
+    //          http status 204
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
